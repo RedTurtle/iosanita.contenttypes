27127a81c8d2bc48e6404c783eba3b331d9fd84b
Daniele Andreotti <daniele.andreotti@redturtle.it>
2024-07-01 13:01:16 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -16,11 +16,12 @@ long_description = "\n\n".join(
 
 setup(
     name="iosanita.contenttypes",
-    version="1.0a1",
+    version="2.0.0",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
