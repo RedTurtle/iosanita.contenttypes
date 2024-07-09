32556c0cf9d307ce5685f962c242c937c72226e1
Daniele Andreotti <daniele.andreotti@redturtle.it>
2024-07-09 11:46:36 +0200

    added tests for Event (WIP)

--- src/iosanita/contenttypes/testing.py
+++ src/iosanita/contenttypes/testing.py
@@ -7,10 +7,28 @@ from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.testing import z2
 
+from redturtle.volto.testing import RedturtleVoltoLayer
+from redturtle.volto.testing import RedturtleVoltoRestApiLayer
+from zope.configuration import xmlconfig
+
+import collective.address
+import collective.taxonomy
+
+# import collective.folderishtypes
+import collective.venue
+import collective.volto.blocksfield
+import collective.volto.cookieconsent
+import collective.volto.enhancedlinks
+import collective.z3cform.datagridfield
+import kitconcept.seo
+import plone.app.caching
+import plone.formwidget.geolocation
+import redturtle.volto
+
 import iosanita.contenttypes
 
 
-class IosanitaContenttypesLayer(PloneSandboxLayer):
+class IosanitaContenttypesLayer(RedturtleVoltoLayer):
 
     defaultBases = (PLONE_FIXTURE,)
 
@@ -18,15 +36,24 @@ class IosanitaContenttypesLayer(PloneSandboxLayer):
         # Load any other ZCML that is required for your tests.
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
-        import plone.app.dexterity
-
-        self.loadZCML(package=plone.app.dexterity)
-        import plone.restapi
-
-        self.loadZCML(package=plone.restapi)
-        self.loadZCML(package=iosanita.contenttypes)
+        super().setUpZope(app, configurationContext)
+        self.loadZCML(package=collective.venue)
+        self.loadZCML(package=collective.volto.blocksfield)
+        self.loadZCML(package=collective.volto.enhancedlinks)
+        self.loadZCML(package=iosanita.contenttypes, context=configurationContext)
+        self.loadZCML(package=plone.formwidget.geolocation)
+        #self.loadZCML(name="overrides.zcml", package=iosanita.contenttypes)
+        xmlconfig.file(
+            "configure.zcml",
+            iosanita.contenttypes,
+            context=configurationContext,
+        )
+        self.loadZCML(package=kitconcept.seo)
+        self.loadZCML(package=collective.taxonomy)
+        self.loadZCML(package=collective.z3cform.datagridfield)
 
     def setUpPloneSite(self, portal):
+        super().setUpPloneSite(portal)
         applyProfile(portal, "iosanita.contenttypes:default")
 
 
@@ -52,4 +79,40 @@ IOSANITA_CONTENTTYPES_ACCEPTANCE_TESTING = FunctionalTesting(
         z2.ZSERVER_FIXTURE,
     ),
     name="IosanitaContenttypesLayer:AcceptanceTesting",
+)
+
+
+class IoSanitaContenttypesRestApiLayer(RedturtleVoltoRestApiLayer):
+    def setUpZope(self, app, configurationContext):
+        super().setUpZope(app, configurationContext)
+        self.loadZCML(package=collective.venue)
+        self.loadZCML(package=collective.volto.blocksfield)
+        self.loadZCML(package=collective.volto.enhancedlinks)
+        self.loadZCML(package=iosanita.contenttypes, context=configurationContext)
+        self.loadZCML(package=plone.formwidget.geolocation)
+        self.loadZCML(package=collective.taxonomy)
+        self.loadZCML(package=collective.z3cform.datagridfield)
+        xmlconfig.file(
+            "configure.zcml",
+            iosanita.contenttypes,
+            context=configurationContext,
+        )
+        self.loadZCML(package=kitconcept.seo)
+
+        #self.loadZCML(name="overrides.zcml", package=iosanita.contenttypes)
+
+    def setUpPloneSite(self, portal):
+        super().setUpPloneSite(portal)
+        applyProfile(portal, "iosanita.contenttypes:default")
+
+
+IOSANITA_CONTENTTYPES_API_FIXTURE = IoSanitaContenttypesRestApiLayer()
+IOSANITA_CONTENTTYPES_API_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(IOSANITA_CONTENTTYPES_API_FIXTURE,),
+    name="DesignPloneContenttypesRestApiLayer:Integration",
+)
+
+IOSANITA_CONTENTTYPES_API_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(IOSANITA_CONTENTTYPES_API_FIXTURE, z2.ZSERVER_FIXTURE),
+    name="DesignPloneContenttypesRestApiLayer:Functional",
 )
