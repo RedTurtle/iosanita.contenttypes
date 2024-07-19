c1380193a8bf4cc9ccb8799dbaf18aa61aec8f75
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-07-19 16:57:19 +0200

    force taxonomy metadata to always be returned in summary serializer

---
+++ src/iosanita/contenttypes/restapi/serializers/summary.py
@@ -0,0 +1,11 @@
+from plone.restapi.interfaces import IJSONSummarySerializerMetadata
+from zope.interface import implementer
+
+
+@implementer(IJSONSummarySerializerMetadata)
+class JSONSummarySerializerMetadata:
+    def default_metadata_fields(self):
+        """
+        Force always return some metadata
+        """
+        return {"parliamo_di_metadata", "a_chi_si_rivolge_tassonomia_metadata"}
---
+++ src/iosanita/contenttypes/tests/test_serializer_summary.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""Setup tests for this package."""
+from iosanita.contenttypes.testing import RESTAPI_TESTING
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from plone.restapi.testing import RelativeSession
+from transaction import commit
+
+import unittest
+
+
+class TestSerializerSummary(unittest.TestCase):
+    """"""
+
+    layer = RESTAPI_TESTING
+
+    def setUp(self):
+        self.app = self.layer["app"]
+        self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+
+        self.request["LANGUAGE"] = "it"
+
+        self.portal_url = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.api_session = RelativeSession(self.portal_url)
+        self.api_session.headers.update({"Accept": "application/json"})
+        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+
+    def test_summary_serializer_always_return_parliamo_di_metadata(self):
+        """ """
+        struttura = api.content.create(
+            container=self.portal,
+            type="Struttura",
+            title="Test servizio",
+            parliamo_di=["ticket-ed-esenzioni", "igiene-pubblica"],
+        )
+        commit()
+
+        resp = self.api_session.get(f"@search?UID={struttura.UID()}").json()
+        self.assertEqual(resp["items_total"], 1)
+        self.assertIn("parliamo_di_metadata", resp["items"][0])
+        self.assertEqual(
+            resp["items"][0]["parliamo_di_metadata"],
+            [
+                {"title": "Ticket ed esenzioni", "token": "ticket-ed-esenzioni"},
+                {"title": "Igiene Pubblica", "token": "igiene-pubblica"},
+            ],
+        )
+
+    def test_summary_serializer_always_return_a_chi_si_rivolge_tassonomia_metadata(
+        self,
+    ):
+        """ """
+        struttura = api.content.create(
+            container=self.portal,
+            type="Struttura",
+            title="Test servizio",
+            a_chi_si_rivolge_tassonomia=[
+                "farmacie",
+                "imprese",
+            ],
+        )
+
+        commit()
+        resp = self.api_session.get(f"@search?UID={struttura.UID()}").json()
+
+        self.assertEqual(resp["items_total"], 1)
+        self.assertIn("a_chi_si_rivolge_tassonomia_metadata", resp["items"][0])
+        self.assertEqual(
+            resp["items"][0]["a_chi_si_rivolge_tassonomia_metadata"],
+            [
+                {"title": "Farmacie", "token": "farmacie"},
+                {"title": "Imprese", "token": "imprese"},
+            ],
+        )
