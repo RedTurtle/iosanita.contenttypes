0737cf2db4bebfdfd7e669142febbb6ac8dc2c2f
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-08-02 14:09:47 +0200

    fix step schema

--- src/iosanita/contenttypes/behaviors/dove.py
+++ src/iosanita/contenttypes/behaviors/dove.py
@@ -3,17 +3,17 @@ from collective.address.behaviors import IAddress
 from collective.geolocationbehavior.geolocation import IGeolocatable
 from iosanita.contenttypes import _
 from plone.app.dexterity import textindexer
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.autoform import directives as form
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.interfaces import IDexterityContent
 from plone.supermodel import model
+from z3c.relationfield.schema import RelationChoice
+from z3c.relationfield.schema import RelationList
 from zope import schema
 from zope.component import adapter
 from zope.interface import implementer
 from zope.interface import provider
-from z3c.relationfield.schema import RelationChoice
-from z3c.relationfield.schema import RelationList
-from plone.autoform import directives as form
-from plone.app.z3cform.widget import RelatedItemsFieldWidget
 
 
 class IDoveSchema(IGeolocatable, IAddress):
--- src/iosanita/contenttypes/tests/test_ct_step.py
+++ src/iosanita/contenttypes/tests/test_ct_step.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
-from iosanita.contenttypes.testing import INTEGRATION_TESTING
 from iosanita.contenttypes.testing import RESTAPI_TESTING
 from plone import api
 from plone.app.testing import setRoles
