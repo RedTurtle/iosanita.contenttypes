a16f60c98b280a324358dbc521a3b18746a37750
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-08-12 17:42:17 +0200

    fix event fields

--- src/iosanita/contenttypes/behaviors/dove.py
+++ src/iosanita/contenttypes/behaviors/dove.py
@@ -3,13 +3,9 @@ from collective.address.behaviors import IAddress
 from collective.geolocationbehavior.geolocation import IGeolocatable
 from iosanita.contenttypes import _
 from plone.app.dexterity import textindexer
-from plone.app.z3cform.widget import RelatedItemsFieldWidget
-from plone.autoform import directives as form
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.interfaces import IDexterityContent
 from plone.supermodel import model
-from z3c.relationfield.schema import RelationChoice
-from z3c.relationfield.schema import RelationList
 from zope import schema
 from zope.component import adapter
 from zope.interface import implementer
@@ -59,45 +55,6 @@ class IDove(IGeolocatable, IAddress):
             "geolocation",
         ],
     )
-
-
-# @provider(IFormFieldProvider)
-# class IDove(IDoveSchema):
-#     struttura_correlata = RelationList(
-#         title=_("struttura_correlata_label", default="Struttura di riferimento"),
-#         description=_(
-#             "struttura_correlata_help",
-#             default="Indicare una o più strutture che fanno a capo a questo contenuto.",
-#         ),
-#         value_type=RelationChoice(vocabulary="plone.app.vocabularies.Catalog"),
-#         default=[],
-#         required=True,
-#     )
-#     form.widget(
-#         "struttura_correlata",
-#         RelatedItemsFieldWidget,
-#         vocabulary="plone.app.vocabularies.Catalog",
-#         pattern_options={"selectableTypes": ["Struttura"]},
-#     )
-#     model.fieldset(
-#         "dove",
-#         label=_("dove_label", default="Dove"),
-#         description=_(
-#             "dove_help",
-#             default="Se la sede di questo contenuto non coincide con la Struttura di riferimento, compilare gli altri campi.",
-#         ),
-#         fields=[
-#             "struttura_correlata",
-#             "nome_sede",
-#             "street",
-#             "zip_code",
-#             "city",
-#             "quartiere",
-#             "circoscrizione",
-#             "country",
-#             "geolocation",
-#         ],
-#     )
 
 
 @implementer(IDove)
