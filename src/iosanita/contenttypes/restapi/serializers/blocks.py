4ebace9dd6683a3625c76a14e19f10ca5b7251c7
Mauro Amico <mauro.amico@gmail.com>
2025-06-26 18:42:20 +0200

    missing file

---
+++ src/iosanita/contenttypes/restapi/serializers/blocks.py
@@ -0,0 +1,49 @@
+from plone.restapi.behaviors import IBlocks
+from plone.restapi.interfaces import IBlockFieldSerializationTransformer
+from zope.component import adapter
+from zope.interface import implementer
+from zope.publisher.interfaces.browser import IBrowserRequest
+from plone.restapi.types.utils import get_info_for_type
+from zope.component import queryMultiAdapter
+from plone import api
+
+
+@implementer(IBlockFieldSerializationTransformer)
+@adapter(IBlocks, IBrowserRequest)
+class SearchTableVariationBlockSerialize:
+    order = 100
+    block_type = "search"
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def _get_schema(self, portal_type):
+        dtool = queryMultiAdapter(
+            (api.portal.get(), self.request), name="dexterity-types"
+        )
+        try:
+            dtype = dtool.publishTraverse(self.request, portal_type)
+        except KeyError:
+            # TODO: log missing portal_type
+            return None
+        schema = get_info_for_type(dtype, self.request, portal_type)
+        return schema
+
+    def __call__(self, value):
+        schemas = {}
+        if value.get("listingBodyTemplate") == "table":
+            for col in value.get("columns") or []:
+                if not col.get("ct") or not col.get("field"):
+                    continue
+                if col["ct"] not in schemas:
+                    # get schema as plone.restapi /@types/Schema                    
+                    schemas[col["ct"]] = self._get_schema(col["ct"])
+                if not schemas[col["ct"]]:
+                    continue
+                schema = schemas[col["ct"]]
+                if "properties" in schema and col["field"] in schema["properties"]:
+                    # TODO: ma servono veramente tutte le info o solo una parte ?
+                    col["field_properties"] = schema["properties"][col["field"]]
+        return value
+
